c022eaf853d583531709b9ccca68f4fb0650ef5d
milistu <studen.m98@gmail.com>
2024-01-08 16:30:40 +0100

    Add token and user-pass auth

--- backend.py
+++ backend.py
@@ -2,27 +2,70 @@ import os
 import io
 import cv2
 import numpy as np
-from typing import Optional, Union
+from jose import JWTError, jwt
 from pydub import AudioSegment
+from pydantic import BaseModel
+from typing import Optional
 from dotenv import load_dotenv, find_dotenv
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse, StreamingResponse
-from fastapi import FastAPI, UploadFile, HTTPException, Depends, Header, File, status
+from fastapi import (
+    FastAPI,
+    UploadFile,
+    HTTPException,
+    Depends,
+    Security,
+    File,
+    status,
+)
+
+from utils import load_user_db, create_access_token, verify_password
 
 load_dotenv(find_dotenv())
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+users_db = load_user_db()
 
-def verify_token(token: str = Depends(oauth2_scheme)):
-    if token != os.environ["MY_API_TOKEN"]:
-        raise HTTPException(
+
+# Define a Pydantic model for the payload of the token
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+def verify_token(token: str = Security(oauth2_scheme), credentials_exception=None):
+    if credentials_exception is None:
+        credentials_exception = HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
+            detail="Could not validate credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    return token
+    try:
+        payload = jwt.decode(token, os.environ["SECRET_KEY"], algorithms=["HS256"])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = users_db.get(token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@app.post("/token")
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = users_db.get(form_data.username)
+    if not user or not verify_password(form_data.password, user["hashed_password"]):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": user["username"]})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.post("/upload/image/")
@@ -73,7 +116,7 @@ async def process_audio(
 @app.post("/upload/text/")
 async def process_text(
     file: Optional[UploadFile] = File(None),
-    text: Optional[str] = File(None),
+    text: Optional[str] = None,
     token: str = Depends(verify_token),
 ):
     if file:
